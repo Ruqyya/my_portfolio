e093f3e477af53495fec5be22520bf7295ff961d
root <mianadee7374@gmail.com>
2026-07-03 05:47:12 +0500

    new changes

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/my_portfolio/',
   plugins: [react(), tailwindcss()],
 })
